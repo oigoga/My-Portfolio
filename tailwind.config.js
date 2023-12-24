0f8ae4c47b7f1ce2df1ac718f267da428a5d0e3a
oigoga <gogaelisabeth21@gmail.com>
2023-12-24 19:28:15 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    colors: {
+      'black': '#222831',
+      'white': '#FFFFFF',
+      'offwhite': '#f2f2f2',
+      'gray': '#333333',
+      'gray2': '#666666',
+      'gray3': '#B8B8B8',
+      'gray4': '#EBEBEB',
+      'grey': '#393e46',
+      'orange': '#f96d00',
+      'orange-2': '#c24d2c',
+      'orange-3': '#ff5722',
+      'transparent': 'transparent'
+
+     
+
+    },
+    fontFamily: {
+      'sans': ['ui-sans-serif', 'system-ui'],
+      'serif': ['ui-serif', 'Georgia'],
+      'Bakbak': ['Bakbak One', 'cursive'],
+      'Urbanist': ['Urbanist', 'sans-serif'],
+      'Inter': ['Inter', 'sans-serif'],
+      
+    },
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
+  },
+  plugins: [],
+}
